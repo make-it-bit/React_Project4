ce6294cb91f8c830f320cc92220e515f17e51171
halothesleeper <artitamm8@gmail.com>
2023-06-14 16:09:21 +0200

    basic design but functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import styles from './App.module.scss';
 
 function App() {
+  const [arrayOfItems, setArrayOfItems] = useState([]);
+  const [newItem, setNewItem] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setArrayOfItems((prevArray) => {
+      return [...prevArray, newItem];
+    });
+    setNewItem('');
+  };
+
+  const changeHandler = (e) => {
+    setNewItem(e.target.value);
+  };
+
+  const deleteListItem = (item) => {
+    setArrayOfItems((list) => {
+     return list.filter((listItem) => listItem !== item);
+    });
+  };
+
+  const ShoppingList = ({deleteSelf}) => {
+    return(
+      <section className={styles.shoppingListSection}>
+        <h2>Your Shopping List</h2>
+        <ul>
+          {arrayOfItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <div>
+                  <span>{item}</span>
+                  <button type="button" onClick={() => deleteSelf(item)}>X</button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    )
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.wholeAppComponent}>
+      <section className={styles.addItemSection}>
+        <h1>Items to buy</h1>
+        <form onSubmit={submitHandler} className={styles.formElement}>
+          <input type='text' value={newItem} placeholder='Add a new item' onChange={changeHandler} required/>
+          <br/>
+          <button>Add</button>
+        </form>
+      </section>
+      {arrayOfItems.length > 0 && <ShoppingList deleteSelf={deleteListItem}/>}
+    </main>
   );
 }
 
